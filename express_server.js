03b9576cf2ef7f6a469c9d9f5a5e6e84a10024fe
sylvielcq <slcq.dev@gmail.com>
2022-08-08 17:29:55 -0700

    Setup EJS

--- express_server.js
+++ express_server.js
@@ -3,12 +3,14 @@ const morgan = require("morgan");
 const app = express();
 const PORT = 8080; // default port 8080
 
+app.use(morgan("dev"));
+
+app.set("view engine", "ejs");
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
-app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   res.send("Hello!");
