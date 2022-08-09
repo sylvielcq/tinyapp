fdba21275aaba468750f4ac75bb36ebcb964fabd
sylvielcq <slcq.dev@gmail.com>
2022-08-08 20:02:00 -0700

    Added Get route, created accompanying EJS form template, added Post route to receive form submission

--- express_server.js
+++ express_server.js
@@ -3,14 +3,16 @@ const morgan = require("morgan");
 const app = express();
 const PORT = 8080; // default port 8080
 
-app.use(morgan("dev"));
-
-app.set("view engine", "ejs");
-
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.use(morgan("dev"));
+
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -29,9 +31,18 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
