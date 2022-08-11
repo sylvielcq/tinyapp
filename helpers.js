d71ec894bf9240f49099c93d24301f5e4947673f
sylvielcq <slcq.dev@gmail.com>
2022-08-11 15:33:28 -0700

    Moved other helper functions to the helpers module

--- helpers.js
+++ helpers.js
@@ -8,4 +8,31 @@ const getUserByEmail = function(email, database) {
   return null;
 };
 
-module.exports = { getUserByEmail };
+
+
+// Helper function that returns only the URLs that were created by the logged in User
+const urlsForUser = function(id, database) {
+  let result = {};
+  for (let url in database) {
+    if (database[url].userID === id) {
+      result[url] = database[url];
+    }
+  }
+  return result;
+};
+
+
+
+// Helper function that generate short URLs IDs
+const generateRandomString = function() {
+  let alphNum = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890';
+  let result = '';
+  for (let i = 0; i < 6; i++) {
+    let j = Math.floor(Math.random() * (alphNum.length - 1));
+    result += alphNum[j];
+  }
+  return result;
+};
+
+
+module.exports = { getUserByEmail, generateRandomString, urlsForUser };
